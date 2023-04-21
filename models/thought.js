d8c2ebbc51fd3f5efc899e506a519c040464849c
dukanegolder <dukanegolder@gmail.com>
2023-04-20 23:32:57 -0600

    added models for all the model files

---
+++ models/thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./reaction");
+const moment = require("moment");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (date) => moment(date).format("LLL"),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
